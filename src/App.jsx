a2072ad3882239ee0ff4d1fca6b2a6f795890f54
Maaz Munir <maazmunir99@gmail.com>
2023-10-03 20:32:42 +0500

    signin page + basic routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+import SignIn from './components/SignIn'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello World </h1>
-    </>
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+    </Routes>
   )
 }
 
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,94 @@
+import{ useState } from 'react';
+
+import logo from '../assets/logo.png';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    // Add your sign-in logic here
+  };
+
+  return (
+    <div className= "min-h-screen flex items-center justify-center bg-yellow-100">
+      <div className="max-w-md w-250 space-y-8">
+        <div className='my-0'>
+          <img className='' src={logo}/>
+        </div>
+        <div>
+          <h2 className="mb-10 text-center text-3xl font-extrabold text-gray-900">
+            Log in to your account
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSignIn}>
+          <div className="rounded-md shadow-sm -space-y-px ">
+            <div className='my-5'> 
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div className='my-5'>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                Remember me
+              </label>
+            </div>
+
+            <div className="text-sm">
+              <a href="#" className="font-medium text-amber-900 hover:text-amber-600">
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-amber-600 hover:bg-amber-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
